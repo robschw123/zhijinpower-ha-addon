e3fbc79ed73a4e2788c19bf0359c614b7046fd80
robschw123 <125215719+robschw123@users.noreply.github.com>
2025-07-12 17:27:39 +0200

    Update api.py
    token nicht ziehen sondern übergeben

--- zhijinpower/api.py
+++ zhijinpower/api.py
@@ -1,17 +1,14 @@
 import requests
-import os
 import traceback
 
-HEADERS = {
-    "platform": "App",
-    "langtype": "en",
-    "token": os.getenv("TOKEN"),
-    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 18_5 like Mac OS X) Html5Plus/1.0 uni-app"
-}
-
-print(f"[DEBUG] Headers: {HEADERS}")
-
-def get_mach_info(machine_id: str) -> dict:
+def get_mach_info(token: str, machine_id: str) -> dict:
+    HEADERS = {
+        "platform": "App",
+        "langtype": "en",
+        "token": token,
+        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 18_5 like Mac OS X) Html5Plus/1.0 uni-app"
+    }    
+    print(f"[DEBUG] Headers: {HEADERS}")
     url = f"http://app.gz529.com/index.php/api/Machine/getMachInfo?id={machine_id}"
     try:
         resp = requests.get(url, headers=HEADERS, params={"id": machine_id}, timeout=10)

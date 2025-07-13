6adc0d5365ff16aca9830191394b31e57f10032e
robschw123 <125215719+robschw123@users.noreply.github.com>
2025-07-13 13:29:23 +0200

    Update run.sh
    Erweiterung

--- zhijinpower/run.sh
+++ zhijinpower/run.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bashio
+set -e
+
+# Konfigurationswerte importieren
 TOKEN=$(bashio::config 'TOKEN')
 MACHINE_ID=$(bashio::config 'MACHINE_ID')
+MQTT_HOST=$(bashio::config 'MQTT_HOST')
+MQTT_PORT=$(bashio::config 'MQTT_PORT')
 MQTT_USER=$(bashio::config 'MQTT_USER')
 MQTT_PASS=$(bashio::config 'MQTT_PASS')
-export TOKEN MACHINE_ID MQTT_USER MQTT_PASS
+
+# In die Environment exportieren
+export TOKEN MACHINE_ID MQTT_HOST MQTT_PORT MQTT_USER MQTT_PASS
+
+echo "Starting ZhijinPower Bridge with TOKEN: ${TOKEN}"
+
 exec python3 /app/main.py
